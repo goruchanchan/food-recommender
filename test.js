0fc4e6d5f229261d12fc953785e16d1e953a6634
goruchan <ppcfr481@yahoo.co.jp>
2023-01-26 23:23:54 +0900

    update 表示すべき項目を表示してみた

--- test.js
+++ test.js
@@ -2,7 +2,7 @@
 
 import fetch from 'node-fetch';
 const API_KEY = '';
-const KEYWORD = 'らんまん食堂 恵比寿';
+const KEYWORD = '';
 
 // http://webservice.recruit.co.jp/hotpepper/gourmet/v1/
 const URL = 'http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=' + API_KEY + '&keyword=' + encodeURIComponent(KEYWORD) + '&format=json';
@@ -17,9 +17,15 @@ async function main() {
     const data = await res.text();
     console.log(data)
     const obj = JSON.parse(data)
-    console.log('店名:' +obj.results.shop[0].name)
-    console.log('住所:' +obj.results.shop[0].address)
-    console.log('URL ' +obj.results.shop[0].urls.pc)
+
+    obj.results.shop.forEach(element => {
+      console.log('店名:' +element.name)
+      console.log('PR:' +element.catch)
+      console.log('ジャンル:' +element.genre.name)
+      console.log('住所:' +element.address)
+      console.log('URL ' +element.urls.pc)
+      console.log('--------------')
+    });
   } catch (err) {
     console.error(err);
   }
