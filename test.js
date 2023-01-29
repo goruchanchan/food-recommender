c4e039105dc5f47505ce8b9f7729ba6a3b27b189
goruchan <ppcfr481@yahoo.co.jp>
2023-01-29 23:05:48 +0900

    update APIキー関連を実行スクリプトへ移動
    APIキー関連をパブリック公開されるnpmに含めるのはまずいので、そういったものは使う側で用意するように構成を変更

--- test.js
+++ test.js
@@ -1,34 +1,38 @@
-#!/usr/bin/env node
-
 import fetch from 'node-fetch';
-const API_KEY = '';
-const KEYWORD = '';
 
-// http://webservice.recruit.co.jp/hotpepper/gourmet/v1/
-const URL = 'http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=' + API_KEY + '&keyword=' + encodeURIComponent(KEYWORD) + '&format=json';
-// console.log(URL);
+class Inquiry{
 
-async function main() {
-  try {
-    const res = await fetch(URL);
-    if (!res.ok) {
-      throw new Error(`${res.status} ${res.statusText}`);
-    }
-    const data = await res.text();
-    console.log(data)
-    const obj = JSON.parse(data)
-
-    obj.results.shop.forEach(element => {
-      console.log('店名:' +element.name)
-      console.log('PR:' +element.catch)
-      console.log('ジャンル:' +element.genre.name)
-      console.log('住所:' +element.address)
-      console.log('URL ' +element.urls.pc)
-      console.log('--------------')
-    });
-  } catch (err) {
-    console.error(err);
+  constructor(api_key, keyword){
+    this.api_key = api_key;
+    this.keyword = keyword;
+    this.url = 'http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=' + api_key + '&keyword=' + encodeURIComponent(keyword) + '&format=json';
+    this.loadShopData();
   }
+
+  async loadShopData() {
+    try {
+      const res = await fetch(this.url);
+      if (!res.ok) {
+        throw new Error(`${res.status} ${res.statusText}`);
+      }
+      const data = await res.text();
+      console.log(data)
+      const obj = JSON.parse(data)
+  
+      obj.results.shop.forEach(element => {
+        console.log('店名:' +element.name)
+        console.log('PR:' +element.catch)
+        console.log('ジャンル:' +element.genre.name)
+        console.log('住所:' +element.address)
+        console.log('URL ' +element.urls.pc)
+        console.log('--------------')
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
 }
 
-main();
+export default Inquiry;
+
