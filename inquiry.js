daa269db0b4e2b406ad15713fa32329555dca88f
goruchan <ppcfr481@yahoo.co.jp>
2023-02-17 15:22:56 +0900

    clean コード整理

--- inquiry.js
+++ inquiry.js
@@ -5,10 +5,8 @@ class Inquiry{
 
   constructor(){
     this.option = minimist(process.argv.slice(2));
-    console.log(this.option);
-    const api_key = this.option.k;
-    const address = this.option.a;
-    this.url = 'http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=' + api_key + '&address=' + encodeURIComponent(address) + '&format=json';
+    this.url = 'http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=' + this.option.k + '&address=' + encodeURIComponent(this.option.a) + '&format=json' + '&count=100';
+    this.genre = [];
     this.loadShopData();
   }
 
@@ -19,19 +17,27 @@ class Inquiry{
         throw new Error(`${res.status} ${res.statusText}`);
       }
       const data = await res.text();
-      // console.log(data)
-      const obj = JSON.parse(data)
-  
-      obj.results.shop.forEach(element => {
-        console.log('店名:' +element.name)
-        console.log('PR:' +element.catch)
-        console.log('ジャンル:' +element.genre.name)
-        console.log('住所:' +element.address)
-        console.log('URL ' +element.urls.pc)
-        console.log('--------------')
+
+      this.shop = JSON.parse(data).results.shop;
+      this.shop.forEach(element => {
+        if (!this.genre.includes(element.genre.name)) {
+          this.genre.push(element.genre.name);
+        }
       });
+  
+      // obj.results.shop.forEach(element => {
+      //   console.log('店名:' +element.name);
+      //   console.log('PR:' +element.catch);
+      //   console.log('ジャンル:' +element.genre.name);
+      //   console.log('住所:' +element.address);
+      //   console.log('URL ' +element.urls.pc);
+      //   console.log('--------------');
+      //   if (!this.genre.includes(element.genre.name)) {
+      //     this.genre.push(element.genre.name);
+      //   }
+      // });
     } catch (err) {
-      console.error(err);
+      console.error("err");
     }
   }
 
