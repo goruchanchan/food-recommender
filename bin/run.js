143d19f3c98b6c38d0820399109c0d370f3ffa11
goruchan <ppcfr481@yahoo.co.jp>
2023-03-13 23:28:23 +0900

    add ショップ一覧とランダム出力追加

--- bin/run.js
+++ bin/run.js
@@ -1,5 +1,9 @@
 #!/usr/bin/env node
 
-import Inquiry from "../inquiry.js"
+import Inquiry from "../lib/inquiry.js";
+import City from "../lib/city.js";
+import Patent from "../lib/patent.js";
 
 new Inquiry();
+// new City();
+// new Patent();
